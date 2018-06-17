29f46febd96b322de3fb14d541b89866cd14aa5f
bodze <bodze@DESKTOP-DDKGC2I>
2018-06-17 08:39:55 +0200

    Ajoutez des fichiers projet.

---
+++ SPFileZilla/FormAbout.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SPFileZilla2013
+{
+    public partial class FormAbout : Form
+    {
+
+        public FormAbout()
+        {
+            InitializeComponent();
+            btnOk.Focus();
+            textBox1.Select(0, 0);
+            pbFileZilla.Click += new EventHandler(pbFileZilla_Click);
+
+            lblVersionNumber.Text = BandR.Consts.VERSION;
+        }
+
+        void pbFileZilla_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://filezilla-project.org/");
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+    }
+}
---
+++ SPFileZilla/classes/Consts.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BandR
+{
+    public static class Consts
+    {
+
+        public const int MAX_ROW_LIMIT = 5000;
+
+        public const string VERSION = "2.1"; // #changeme
+
+    }
+}
